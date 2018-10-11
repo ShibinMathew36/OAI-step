0f8c92a1385b7cc2007ff1d1eb43f022750dd43c
ShibinMathew36 <smathe36@uic.edu>
2018-10-11 15:15:04 -0500

    cqi log 2

--- openair2/LAYER2/MAC/pre_processor.c
+++ openair2/LAYER2/MAC/pre_processor.c
@@ -31,6 +31,7 @@
 
 #define _GNU_SOURCE
 #include <stdlib.h>
+#include <sys/time.h>
 
 #include "assertions.h"
 #include "PHY/defs.h"
@@ -210,7 +211,10 @@ void assign_rbs_required (module_id_t Mod_id,
       eNB_UE_stats[CC_id]->DL_cqi[0], MIN_CQI_VALUE, MAX_CQI_VALUE);
       */
       eNB_UE_stats[CC_id]->dlsch_mcs1=cqi_to_mcs[eNB_UE_stats[CC_id]->DL_cqi[0]];
-      LOG_I(MAC,"\n CQI value for UE %d is %d ",UE_id, eNB_UE_stats[CC_id]->DL_cqi[0]);
+      struct timeval  te;
+      gettimeofday(&te, NULL);
+      long long milliseconds = te.tv_sec*1000LL + te.tv_usec/1000;
+      LOG_I(MAC,"Time %lld CQI value for UE %d is %d \n",milliseconds, UE_id, eNB_UE_stats[CC_id]->DL_cqi[0]);
       eNB_UE_stats[CC_id]->dlsch_mcs1 = eNB_UE_stats[CC_id]->dlsch_mcs1;//cmin(eNB_UE_stats[CC_id]->dlsch_mcs1,openair_daq_vars.target_ue_dl_mcs);
 
     }
